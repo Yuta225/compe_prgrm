ae62d7b16e3f29452d762264bb4458d94db81b31
wkznyt(m) <leonee0510@gmail.com>
2022-10-29 23:32:41 +0900

    [add] abc275

---
+++ ABC275/a.py
@@ -0,0 +1,3 @@
+N = int(input())
+p = list(map(int,input().split()))
+print(p.index(max(p))+1)
--- 入力.py
+++ 入力.py
@@ -2,3 +2,5 @@ N = int(input())
 N,W = map(int, input().split())
 p = list(map(int,input().split()))
 p = [tuple(map(int, input().split())) for i in range(N)]
+
+s = [input() for _ in range(9)]
